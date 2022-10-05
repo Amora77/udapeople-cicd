fac2733b7d9dff143b51c30c000b1d66580b4114
Amora77 <50799209+Amora77@users.noreply.github.com>
2022-10-05 12:34:25 +0200

    sd

--- .circleci/ansible/roles/configure-server/files/myenv.sh
+++ .circleci/ansible/roles/configure-server/files/myenv.sh
@@ -1,9 +1,9 @@
 TYPEORM_CONNECTION=postgres
-TYPEORM_ENTITIES=./src/modules/domain/**/*.entity.ts
+TYPEORM_ENTITIES=./modules/domain/**/*.entity.ts
 TYPEORM_HOST=database-1.c14rdikvngy9.us-west-2.rds.amazonaws.com
 TYPEORM_PORT=5432
 TYPEORM_USERNAME=postgres
 TYPEORM_PASSWORD=postgres
 TYPEORM_DATABASE=postgres
-TYPEORM_MIGRATIONS=./src/migrations/*.ts
-TYPEORM_MIGRATIONS_DIR=./src/migrations
+TYPEORM_MIGRATIONS=./migrations/*.ts
+TYPEORM_MIGRATIONS_DIR=./migrations
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -47,7 +47,7 @@ async function bootstrap() {
   };
   app.use(cors({  "origin": "*", "methods": "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS"}));
   app.useGlobalFilters(new ErrorFilter());
-  //await app.listen(config.PORT);
+  await app.listen(config.PORT);
   logger.log(`Listening on port ${config.PORT}.`);
 }
 
